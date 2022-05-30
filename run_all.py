182e0b8c6dcbaa4e8c6171f74271342566d4090a
Matteo Chen <kaiximatteo.chen@huxelerate.it>
2022-05-30 23:03:28 +0200

    script for all tests

---
+++ run_all.py
@@ -0,0 +1,26 @@
+import os
+import filecmp
+
+ERROR = "[ERROR]: "
+INFO  = "[INFO] : "
+
+ret = os.system("clear && gcc -std=c11 main.c")
+if ret:
+    print(ERROR + "compilation erro")
+
+tests = os.listdir("./tests")
+
+ins = {}
+for test in tests:
+    if not test.endswith(".output.txt"):
+        ins[f'{test[0:len(test)-len(".txt")]}'] = {"in": test, "out": test[0:len(test)-len(".txt")]+".output.txt"}
+
+for test in ins:
+    print(ins[test]["in"])
+    ret = os.system("./a.out <" + f'./tests/{ins[test]["in"]}')
+    if ret:
+        print(ERROR + "Failed to run test: " + test)
+    print(INFO + "Test: " + test + " result: " + str(filecmp.cmp(f'./tests/{ins[test]["out"]}', "out.txt", shallow=False)))
+    os.system(f'mv out.txt out/{test}_out.txt')
+
+
